ebd4594e69eee4d98c2b4958e2853cb7aa7354e5
Michal Boczula <michal.boczula0101@gmail.com>
2026-07-20 00:01:24 +0200

    Added YARP

--- src/ECommerceStoreBFF.API/Program.cs
+++ src/ECommerceStoreBFF.API/Program.cs
@@ -1,29 +1,43 @@
 using Scalar.AspNetCore;
 using ECommerceStoreBFF.Infrastructure;
-using ECommerceStoreBFF.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructureServices();
-builder.Services.AddOpenApi();
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddHttpClient();
-
 var app = builder.Build();
 
-app.MapOpenApi("swagger/v1/swagger.json");
-app.MapScalarApiReference(options =>
+app.MapScalarApiReference("scalar/products", options =>
 {
-    options.WithTitle("ECommerce BFF API")
-            .WithTheme(ScalarTheme.DeepSpace)
-            .WithOpenApiRoutePattern("/swagger/v1/swagger.json");
+    options.WithTitle("ECommerce BFF - Products API")
+           .WithTheme(ScalarTheme.DeepSpace)
+           .WithOpenApiRoutePattern("/api/products/swagger/v1/swagger.json");
 });
 
-app.MapUsersEndpoints();
-app.MapProductsEndpoints();
+app.MapScalarApiReference("scalar/users", options =>
+{
+    options.WithTitle("ECommerce BFF - Users API")
+           .WithTheme(ScalarTheme.BluePlanet)
+           .WithOpenApiRoutePattern("/api/users/swagger/v1/swagger.json");
+});
+
+app.MapScalarApiReference("scalar/orders", options =>
+{
+    options.WithTitle("ECommerce BFF - Orders API")
+           .WithTheme(ScalarTheme.Mars)
+           .WithOpenApiRoutePattern("/api/orders/swagger/v1/swagger.json");
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[BFF Request Received] Path: {context.Request.Path}{context.Request.QueryString}");
+
+    await next();
+
+    Console.WriteLine($"[BFF Request Finished] Status Code: {context.Response.StatusCode}");
+});
 
 app.MapReverseProxy();
-
 app.Run();
